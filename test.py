4713c69d4c63f192143fe26298838a7a8375203f
Iordache Paul-Tiberiu <123265734+PaulTiberiu@users.noreply.github.com>
2023-11-21 21:43:44 +0100

    Update test.py

--- test.py
+++ test.py
@@ -12,16 +12,48 @@ s = Graph.Graph.glouton_fas(graph)
 print(s)
 print("")
 
-
-bg, arbre, iter = Graph.Graph.bellmanFord(graph, 1)
-print("Distances des plus court chemin de 1: ", bg)
-print("Plus court chemin en partant de 1", arbre)
-print("Le nombre d'iterations: ", iter)
-
+v = [1, 2, 3, 4]
+a = {1 : {(2,-4)}, 2 : {(3, -8)}, 3 : {(1, 7), (4, 2)}}
+negative_graph = Graph.Graph(v, a) 
+print("Graphe cycle negatif: ")
+print("Sommets:", negative_graph.V)
+print("Arcs:", negative_graph.E)
 print("")
 
-bg, arbre, iter = Graph.Graph.bellmanFordMVP(graph, 1)
+bg, arbre, iter = Graph.Graph.bellmanFord(negative_graph, 1)
 print("Distances des plus court chemin de 1: ", bg)
-print("Plus court chemin en partant de 1", arbre)
+print("Plus court chemin en partant de 1: ", arbre)
 print("Le nombre d'iterations: ", iter)
+print("")
+
+
+graph_rand = Graph.Graph.random_graph_unary_weight(5, 0.3)
+print("Graphe random: ")
+print("Sommets:", graph_rand.V)
+print("Arcs:", graph_rand.E)
+print("")
+
+dico = Graph.Graph.out_degrees(graph_rand)
+deg = Graph.Graph.node_with_high_out_degree(graph_rand)
+print("Sommet avec degree sortant > |V|/2: ", deg)
+
+graph_rand_w1 = Graph.Graph.weighed_graph(graph_rand, 3)
+graph_rand_w2 = Graph.Graph.weighed_graph(graph_rand, 5)
+graph_rand_w3 = Graph.Graph.weighed_graph(graph_rand, 10)
+
+print("Graphe random w1: ")
+print("Sommets:", graph_rand_w1.V)
+print("Arcs:", graph_rand_w1.E)
+print("")
+
+print("Graphe random w2: ")
+print("Sommets:", graph_rand_w2.V)
+print("Arcs:", graph_rand_w2.E)
+print("")
+
+print("Graphe random w3: ")
+print("Sommets:", graph_rand_w3.V)
+print("Arcs:", graph_rand_w3.E)
+print("")
+
 
